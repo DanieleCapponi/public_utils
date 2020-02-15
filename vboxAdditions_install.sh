f85f6d090ecc5991ed61665c1df18c30132837e4
Daniele <d.danielecapponi@gmail.com>
2020-02-15 19:51:20 +0100

    Comments

--- vboxAdditions_install.sh
+++ vboxAdditions_install.sh
@@ -1,6 +1,9 @@
+# TESTED ON UBUNTU 12.04 > 16.04# 
+#
 # In the menu of your VirtualBox VM:
 # Devices -> Insert Guest Additions CD Image
 # In you host:
+
 mkdir /media/cdrom
 sudo mount /dev/cdrom /media/cdrom
 sudo apt-get install -y dkms build-essential linux-headers-generic linux-headers-$(uname -r)
